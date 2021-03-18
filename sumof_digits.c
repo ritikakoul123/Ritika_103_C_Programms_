67f212e395bf2cea2514a808dbbe5a70015d0c21
ritikakoul123 <76279413+ritikakoul123@users.noreply.github.com>
2021-03-18 20:40:33 +0530

    Update sumof_digits.c

--- sumof_digits.c
+++ sumof_digits.c
@@ -1,7 +1,7 @@
 #include<stdio.h>  // Pre-Prossive to include standard input and out put header files funtions
  int main()    
-{    // Start Of mail body
-int n,sum=0,m;    
+{    // Start of mail body
+int n,sum=0,m;     
 printf("Enter a number:");    
 scanf("%d",&n);    
 while(n>0)    // Loop started 
