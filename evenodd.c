262a1073d52dee6ca1122c7d9dfc543033ef4b51
ritikakoul123 <76279413+ritikakoul123@users.noreply.github.com>
2021-03-21 14:52:03 +0530

    Even and odd Program added

--- evenodd.c
+++ evenodd.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> // Pre-Processive to include standard input and output header files 
 int main() {
     int num;
-    printf("Enter an integer: ");
+    printf("Enter an integer: "); // Input from user 
     scanf("%d", &num);
 
     // True if num is perfectly divisible by 2
-    if(num % 2 == 0)
+    if(num % 2 == 0) // Logic for even odd 
         printf("%d is even.", num);
     else
         printf("%d is odd.", num);
