0f11c8a5eeffe46cc8f055b8718fb5eaa0aec3ec
ritikakoul123 <76279413+ritikakoul123@users.noreply.github.com>
2021-03-21 14:46:16 +0530

    Prime number using while loop program added

--- Prime_While.c
+++ Prime_While.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // Pro-Prossive to include standard input output header files funtions
 int main()
 {
-   int a, i, c=0;
- 	printf("Enter number to check prime number or not");
+   int a, i, c=0; // Variable Declration 
+ 	printf("Enter number to check prime number or not"); // input from user 
     scanf("%d",&a);
     i=2;
     while( i<=a/2)
@@ -17,12 +17,12 @@ int main()
     }
  
     if (c==0)
-        printf("%d is a prime number.",a);
+        printf("%d is a prime number.",a); // Print Prime if its true 
     else
-        printf("%d is not a prime number.",a);
+        printf("%d is not a prime number.",a); // Print Not Prime if its False
  
     return 0;
-    
+    // End of Program 
 }
  
 
