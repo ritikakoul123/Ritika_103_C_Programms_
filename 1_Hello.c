48e75bedb98ed866e7f2324749592a69d1ca3b8d
ritikakoul123 <76279413+ritikakoul123@users.noreply.github.com>
2021-03-23 20:15:53 +0530

    Delete 1_Hello.c

--- 1_Hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h> // Pre-Processive to include standard input and output header files funtions 
-int main() // Start of body
-{
-	printf("HEllO WORLD I m KAVYA DHAR"); // Printing statement
-	return 0;
-} //  end of body 
